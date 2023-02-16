5cce02e99837db2219cf6813ac27a92f137759a6
kamila <murtazaeva.kamila01@gmail.com>
2023-02-16 19:16:51 +0600

    Initial Commit

---
+++ hw-2-m-4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw-2-m-4
+//
+//  Created by Kamila on 16/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
